b97da4a42ca40c9f2b6f3e8db98e93e3c2721f24
Charles Scalesse <scalessec@gmail.com>
2013-01-23 22:30:13 -0500

    Updated example and reorganized the directory structure

---
+++ Toast/Example/ToastTestViewController.h
@@ -0,0 +1,12 @@
+//
+//  ToastTestViewController.h
+//  ToastTest
+//
+//  Copyright 2013 Charles Scalesse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToastTestViewController : UIViewController
+
+@end
--- Toast/main.m
+++ Toast/main.m
@@ -2,20 +2,14 @@
 //  main.m
 //  ToastTest
 //
-//  Copyright 2012 Charles Scalesse. All rights reserved.
+//  Copyright 2013 Charles Scalesse. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "ToastTestAppDelegate.h"
 
 int main(int argc, char *argv[]) {
-    #if __has_feature(objc_arc)
-        @autoreleasepool {
-            return UIApplicationMain(argc, argv, nil, nil);
-        }
-    #else
-        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-        int retVal = UIApplicationMain(argc, argv, nil, nil);
-        [pool release];
-        return retVal;
-    #endif
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ToastTestAppDelegate class]));
+    }
 }
