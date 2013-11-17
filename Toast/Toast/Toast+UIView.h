ae9b6e18876c045de57ab777264db6ce5dec2f04
Charles Scalesse <scalessec@gmail.com>
2013-11-17 00:02:09 -0500

    Duration properties use NSTimerInterval

--- Toast/Toast/Toast+UIView.h
+++ Toast/Toast/Toast+UIView.h
@@ -34,10 +34,10 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // each makeToast method creates a view and displays it as toast
 - (void)makeToast:(NSString *)message;
-- (void)makeToast:(NSString *)message duration:(CGFloat)interval position:(id)position;
-- (void)makeToast:(NSString *)message duration:(CGFloat)interval position:(id)position image:(UIImage *)image;
-- (void)makeToast:(NSString *)message duration:(CGFloat)interval position:(id)position title:(NSString *)title;
-- (void)makeToast:(NSString *)message duration:(CGFloat)interval position:(id)position title:(NSString *)title image:(UIImage *)image;
+- (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position;
+- (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position image:(UIImage *)image;
+- (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position title:(NSString *)title;
+- (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position title:(NSString *)title image:(UIImage *)image;
 
 // displays toast with an activity spinner
 - (void)makeToastActivity;
@@ -46,6 +46,6 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // the showToast methods display any view as toast
 - (void)showToast:(UIView *)toast;
-- (void)showToast:(UIView *)toast duration:(CGFloat)interval position:(id)point;
+- (void)showToast:(UIView *)toast duration:(NSTimeInterval)interval position:(id)point;
 
 @end
