a28d9d90a9bceb65e90a606e6180947a1c05b171
Charles Scalesse <charles@zola.com>
2016-11-03 21:57:05 -0400

    Add Toast to the framework target

--- Toast/Toast.h
+++ Toast/Toast.h
@@ -16,4 +16,3 @@ FOUNDATION_EXPORT const unsigned char ToastVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Toast/PublicHeader.h>
 
-
